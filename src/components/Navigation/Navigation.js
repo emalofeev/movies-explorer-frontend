e8f5a3625afd3a48c7e4e1cc78745d1b9a2ae753
Evgeniy Malofeev <euge.ma@yandex.ru>
2023-05-24 02:26:48 +0300

    dev: add Promo, AboutProject, Techs, AboutMe, Footer

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-      <div className='navigation'>
-      <Link to='/api/signup' className='navigation__link_register'>
+    <div className='navigation'>
+      <Link to='/api/signup' className='navigation__link-register'>
         Регистрация
       </Link>
-      <Link to='/api/signin' className='navigation__link_login'>
-        <p className='navigation__link_login-text'>Войти</p>
+      <Link to='/api/signin' className='navigation__link-login'>
+        <p className='navigation__link-text'>Войти</p>
       </Link>
-      </div>
+    </div>
   );
 }
 
